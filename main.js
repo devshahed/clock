95725129e21c1a17dde3d3f9862a1151f34b744d
devshahed <62591003+devshahed@users.noreply.github.com>
2021-01-12 17:07:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+showTime();
+function showTime(){
+    var now = new Date;
+    var h = now.getHours();
+    var m = now.getMinutes();
+    var s = now.getSeconds();
+    var format;
+    if(h > 12){
+        format = "PM";
+    }else{
+        format = "AM";
+    }
+    h = (h > 12) ? h - 12 : h;
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    var time = document.getElementById("time");
+    time.innerHTML= h + ':' + m + ':' + s + ' ' + format;
+    setTimeout(showTime, 1000);
+}
+
